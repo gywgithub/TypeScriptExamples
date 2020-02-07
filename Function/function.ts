c08b7bd6c26a3850938906d61b36daa5a94c274e
gyw123 <guoyuanwei@sg-ai.com>
2020-02-07 14:59:29 +0800

    add function

---
+++ Function/function.ts
@@ -0,0 +1,126 @@
+function test() {
+  console.log('test')
+}
+test()
+
+function greet():string {
+  return 'hello world'
+}
+
+function caller() {
+  let msg = greet()
+  console.log(msg)
+}
+
+caller()
+
+function add(x: number, y: number): number {
+  return x + y
+}
+
+console.log(add(1, 2))
+
+function buildName(firstName: string, lastName: string): string {
+  return firstName + ' ' + lastName
+}
+
+console.log(buildName('kyle', 'jack'))
+// console.log(buildName('david')) // function.ts:28:13 - error TS2554: Expected 2 arguments, but got 1.
+// console.log(buildName('abc', '123', '456')) // function.ts:29:37 - error TS2554: Expected 2 arguments, but got 3.
+
+
+function calculate_discount(price:number, rate:number = 0.3) {
+  let discount = price * rate
+  console.log('discount: ', discount)
+  return discount
+}
+
+console.log(calculate_discount(100, 0.5)) // 50
+console.log(calculate_discount(100)) // 30
+
+function buildName2(firstName: string, ...restOfName:string[]) {
+  return firstName + ' ' + restOfName.join(' ')
+}
+console.log(buildName2('Joseph', 'Samuel', 'Kyle')) // Joseph Samuel Kyle
+
+function addNumbers(...nums:number[]) {
+  let i;
+  let sum:number = 0
+  for (i = 0;i < nums.length;i++) {
+    sum += nums[i]
+  }
+  return sum
+}
+
+console.log(addNumbers(1, 2, 3, 4)) // 10
+console.log(addNumbers(10, 20, 10, 20)) // 60
+
+let msg2 = function () {
+  return 'msg2'
+}
+console.log(msg2())
+
+let r = function(x:number, y:number) {
+  let n = x*y
+  return n
+}
+console.log(r(1, 2)) // 2
+
+
+// (function () {
+//   let num = 123
+//   console.log(num)
+// })()
+
+let myFunc = new Function('a', 'b', 'return a*b')
+let x = myFunc(4, 3)
+console.log('x: ', x) // x: 12
+
+
+function factorial(number) {
+  if (number <= 0) {
+    return 1
+  } else {
+    // console.log(number)
+    // console.log(factorial(number - 1))
+    return (number * (factorial(number - 1)))
+  }
+}
+
+console.log(factorial(6)) // 720  
+
+let foo = (x:number) => x + 10
+console.log(foo(100)) // 110
+
+let foo2 = function (x) {
+  x = 10 + x
+  console.log(x) // 210
+}
+foo2(200)
+
+
+let func = (x) => {
+  if (typeof x === 'number') {
+    console.log(x + ' typeof number')
+  } else if (typeof x === 'string') {
+    console.log(x + ' typeof string')
+  }
+}
+func(12)
+func('abc')
+
+let disp = () => {
+  console.log('function invoked')
+}
+disp()
+
+console.log('---')
+
+function disp3(s1:string):void;
+function disp3(n1:number, s1:string):void;
+function disp3(x:any, y?:any) {
+  console.log(x)
+  console.log(y)
+}
+disp3('123')
+disp3(123, 'abc')
